0250efdd1069c2c55cadf7bc0ac63048f3f33d90
Cesar Acual <chechaacual@gmail.com>
2018-08-16 18:54:48 -0600

    20.Desarrollo de una política de acceso con TDD

---
+++ tests/integration/CommentPolicyTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Comment;
+use App\Policies\CommentPolicy;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CommentPolicyTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    function test_the_post_author_can_select_a_comment_as_an_answer()
+    {
+        $comment = factory(Comment::class)->create();
+
+        $policy = new CommentPolicy;
+
+        $this->assertTrue(
+            $policy->accept($comment->post->user, $comment)
+        );
+    }
+
+    function test_non_authors_cannot_select_a_comment_as_an_answer()
+    {
+        $comment = factory(Comment::class)->create();
+
+        $policy = new CommentPolicy;
+
+        $this->assertFalse(
+            $policy->accept(factory(User::class)->create(), $comment)
+        );
+    }
+}
