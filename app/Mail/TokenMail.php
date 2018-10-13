011b91af6aefa2248d5f60c1801787b818ad9a70
Cesar Acual <chechaacual@gmail.com>
2018-10-12 18:31:37 -0600

    29.Registro de usuarios

---
+++ app/Mail/TokenMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use App\Token;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TokenMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    /**
+     * @var Token
+     */
+    public $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Token $token
+     */
+    public function __construct(Token $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
