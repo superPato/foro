1e95b949f4d8c0abb399a711e2243e06069dd168
Cesar Acual <chechaacual@gmail.com>
2018-10-31 11:18:05 -0600

    34.Actualización del proyecto a Laravel 5.4

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -1,11 +1,12 @@
 <?php
 
 use \Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\CreatesApplication;
+use Tests\TestsHelper;
 
-
-class FeatureTestCase extends TestCase
+class FeatureTestCase extends \Laravel\BrowserKitTesting\TestCase
 {
-    use DatabaseTransactions;
+    use CreatesApplication, TestsHelper, DatabaseTransactions;
 
     public function seeErrors(array $fields)
     {
