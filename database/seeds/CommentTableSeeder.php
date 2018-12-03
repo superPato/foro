368c431862a4647dff04e994e5be131719ce7e31
Cesar Acual <chechaacual@gmail.com>
2018-12-03 11:28:05 -0600

    38.Seeders con tablas relacionadas

---
+++ database/seeds/CommentTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Post;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class CommentTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = User::select('id')->get();
+
+        $posts = Post::select('id')->get();
+
+        for ($i = 0; $i < 250; ++$i) {
+            factory(\App\Comment::class)->create([
+                'user_id' => $users->random()->id,
+                'post_id' => $posts->random()->id,
+            ]);
+        }
+    }
+}
