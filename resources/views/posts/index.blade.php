d917a7111d90bb58b55b139c6a52d43afd9427b2
Cesar Acual <chechaacual@gmail.com>
2018-12-20 19:10:18 -0600

    Improve posts list: add basic style and filter by status menu

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>
-        {{ $category->exists ? 'Post de ' . $category->name : 'Posts' }}
-        Posts</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>
+                {{ $category->exists ? 'Post de ' . $category->name : 'Posts' }}
+            </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2">
+            <h4>Filtros</h4>
+            {!! Menu::make(trans('menu.filters'), 'nav filters') !!}
 
-    <ul>
-    @foreach($posts as $post)
-        <li>
-            <a href="{{ $post->url }}">
-                {{ $post->title }}
-            </a>
-        </li>
-    @endforeach
-    </ul>
+            <h4>Categorías</h4>
+            {!! Menu::make($categoryItems, 'nav categories') !!}
+        </div>
+        <div class="col-md-10">
+            <ul>
+                @foreach($posts as $post)
+                    <li>
+                        <a href="{{ $post->url }}">
+                            {{ $post->title }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
 
-    {{ $posts->render() }}
+            {{ $posts->render() }}
+        </div>
+    </div>
 
-    {!! Menu::make($categoryItems, 'nav categories') !!}
 
 @endsection
