9d70ba2c611eb8e5d63da582a082017b7f600b3c
Cesar Acual <chechaacual@gmail.com>
2019-01-07 13:13:45 -0600

    Prueba unitaria para brobar el método owns de User

---
+++ tests/unit/UserModelTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Post;
+use App\User;
+
+class UserModelTest extends TestCase
+{
+    function test_user_owner_of_model_owns_the_model()
+    {
+        $user = new User();
+        $user->id = 1;
+
+        $post = new Post();
+        $post->user_id = $user->id;
+
+        $this->assertTrue($user->owns($post));
+    }
+
+    function test_user_non_owner_of_model_does_not_own_the_model()
+    {
+        $user = new User();
+        $this->id = 1;
+
+        $post = new Post();
+        $post->user_id = 2;
+
+        $this->assertFalse($user->owns($post));
+    }
+}
