6a0b224b60a0031895dc9fbe206266d49c7c8f84
Cesar Acual <chechaacual@gmail.com>
2019-04-11 09:52:33 -0600

    65.Uso de interceptores con axios, Laravel y Vue.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -23,6 +23,19 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Do something with response error
+    if (error.response.status == 401) {
+    	window.location.replace('login');
+    } else {
+    	return Promise.reject(error);
+    }
+  });
+
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
--- tests/integration/APostCanBeVotedTest.php
+++ tests/integration/APostCanBeVotedTest.php
@@ -100,9 +100,9 @@ class APostCanBeVotedTest extends \TestCase
         
         $this->post->upvote();
 
-        $this->assertSame(1, $this->post->current_vote);
-
         $this->post->undoVote();
+
+        $this->post->refresh();
 
         $this->assertNull($this->post->current_vote);
 
